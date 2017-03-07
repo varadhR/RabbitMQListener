f52277aef743383267d8c2cdca0a0352cac2072d
Varadharajan R <rvaradh87@gmail.com>
2017-03-07 22:57:03 +0530

    Update QueueHelper.java

--- src/com/messaging/listener/QueueHelper.java
+++ src/com/messaging/listener/QueueHelper.java
@@ -11,14 +11,15 @@ import com.rabbitmq.client.Envelope;
  * This class creates a consumer and binds it to the queue, so as to read the messages  
  */
 public class QueueHelper { 
-	private MessageProcessor messageProcessor = new TextMessageProcessor();;
+	private MessageProcessor messageProcessor = null;
 	/*
 	 * This method creates a message and binds it to the queue to read the messages
 	 * @Param("channel", used for sending messages to the AMQP Broker)
 	 * @Param("queueName", the queue where the messages have to read )
 	 */
-	public void listenForMessages(Channel channel, String queueName){
+	public void listenForMessages(Channel channel, String queueName, MessageProcessor msgProcessor){
 		try {
+			this.messageProcessor = msgProcessor;
 			channel.basicConsume(queueName, true, new DefaultConsumer(channel){
 				@Override
 				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] message)
