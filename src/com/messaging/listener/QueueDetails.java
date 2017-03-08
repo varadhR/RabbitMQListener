1c52faf6dfcda442206703e8d025061b0ee93cc6
Varadharajan R <rvaradh87@gmail.com>
2017-03-08 11:50:11 +0530

    Update QueueDetails.java

--- src/com/messaging/listener/QueueDetails.java
+++ src/com/messaging/listener/QueueDetails.java
@@ -5,36 +5,17 @@ package com.messaging.listener;
  */		
 public class QueueDetails {
 	
-	private String userName;
-	private String password;
 	private String queueName;
-	private String exchangeName;
 	private String hostName;
 	
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 	public String getQueueName() {
 		return queueName;
 	}
 	public void setQueueName(String queueName) {
 		this.queueName = queueName;
 	}
-	public String getExchangeName() {
-		return exchangeName;
-	}
-	public void setExchangeName(String exchangeName) {
-		this.exchangeName = exchangeName;
-	}
+	
 	
 	public String getHostName() {
 		return hostName;
